a664ea8a34c6c8850dba10ee688975a32d750c3c
Tomas Kara <tomas.kara.91@gmail.com>
2023-03-19 14:26:04 +0100

    some work on charts

---
+++ dashboard/functions.py
@@ -0,0 +1,68 @@
+import plotly.express as px
+
+
+def create_plot(query_set, metric, device):
+    dates = [c.date for c in query_set]
+    metrics = eval(f"[c.{metric}{device} for c in query_set]")
+    clean_metrics = [item for item in metrics if item is not None]
+    if metrics[0] != None:
+        fig = px.line(x=dates, y=metrics)
+        if metric == 'lcp':
+            limits = [2500, 4000]
+        elif metric == 'fid':
+            limits = [100, 300]
+        else:
+            limits = [0.1, 0.25]
+        # add color stripes
+        fig.add_shape(
+            type='rect',
+            x0=min(dates), x1=max(dates),
+            y0=0, y1=limits[0],
+            yref='y',
+            fillcolor='lightgreen',
+            layer='below',
+            opacity=0.4,
+            line_width=0
+        )
+        fig.add_shape(
+            type='rect',
+            x0=min(dates), x1=max(dates),
+            y0=limits[0], y1=limits[1],
+            yref='y',
+            fillcolor='yellow',
+            layer='below',
+            opacity=0.4,
+            line_width=0
+        )
+        fig.add_shape(
+            type='rect',
+            x0=min(dates), x1=max(dates),
+            y0=limits[1], y1=limits[1]+100,
+            yref='y',
+            fillcolor='red',
+            layer='below',
+            opacity=0.4,
+            line_width=0
+        )
+        if metric == 'cls':
+            fig.update_layout(
+                title={
+                    'text': f"{metric}",
+                    'font': {'size': 24}
+                },
+
+                yaxis_range=[min(clean_metrics) - 0.1, max(clean_metrics) + 0.1],
+            )
+        else:
+            fig.update_layout(
+                title={
+                    'text': f"{metric}",
+                    'font': {'size': 24}
+                },
+
+                yaxis_range=[min(clean_metrics) - 5, max(clean_metrics) + 5],
+            )
+        config = dict({'modeBarButtonsToRemove': ['autoScale', 'zoom', 'pan', 'select', 'zoomIn', 'zoomOut']})
+        return fig.to_html(config=config)
+    else:
+        return "Data nejsou k dispozici"
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import plotly.express as px
 from speedcheck.models import Urls, CruxHistory, Profile
+from .functions import create_plot
 
 
 def dashboard(request, url_id):
@@ -19,108 +20,9 @@ def dashboard(request, url_id):
 
     if request.POST.get('addurl'):
         request.user.profile.urls.add(url_object)
-    # lcp graph
-    metric = "lcp"
-    dates = [c.date for c in data]
-    metrics = eval(f"[c.{metric}{device} for c in data]")
-    clean_metrics = [item for item in metrics if item is not None]
-    if metrics[0] != None:
-        fig = px.line(x=dates, y=metrics)
-        limits = [2500, 4000]
-        colors = ['yellow', 'red']
-        # add color stripes
-        fig.add_shape(
-            type='rect',
-            x0=min(dates), x1=max(dates),
-            y0=0, y1=2500,
-            yref='y',
-            fillcolor='lightgreen',
-            layer='below',
-            opacity=0.4,
-            line_width=0
-        )
-        # TODO add color stripes - fig.add_shape
-        fig.update_layout(
-            title={
-                'text': f"{metric}",
-                'font': {'size': 24}
-            },
-
-            yaxis_range=[min(clean_metrics) - 5, max(clean_metrics) + 5],
-        )
-        config = dict({'modeBarButtonsToRemove': ['autoScale', 'zoom', 'pan', 'select', 'zoomIn', 'zoomOut']})
-        chart1 = fig.to_html(config=config)
-    else:
-        chart1 = "Data nejsou k dispozici"
-    
-    # fid graph
-    metric = "fid"
-    dates = [c.date for c in data]
-    metrics = eval(f"[c.{metric}{device} for c in data]")
-    clean_metrics = [item for item in metrics if item is not None]
-    if metrics[0] != None:
-        fig = px.line(x=dates, y=metrics)
-        limits = [2500, 4000]
-        colors = ['yellow', 'red']
-        # add color stripes
-        fig.add_shape(
-            type='rect',
-            x0=min(dates), x1=max(dates),
-            y0=0, y1=2500,
-            yref='y',
-            fillcolor='lightgreen',
-            layer='below',
-            opacity=0.4,
-            line_width=0
-        )
-        # TODO add color stripes - fig.add_shape
-        fig.update_layout(
-            title={
-                'text': f"{metric}",
-                'font': {'size': 24}
-            },
-
-            yaxis_range=[min(clean_metrics) - 5, max(clean_metrics) + 5],
-        )
-        config = dict({'modeBarButtonsToRemove': ['autoScale', 'zoom', 'pan', 'select', 'zoomIn', 'zoomOut']})
-        chart2 = fig.to_html(config=config)
-    else:
-        chart2 = "Data nejsou k dispozici"
-
-    # fid graph
-    metric = "cls"
-    dates = [c.date for c in data]
-    metrics = eval(f"[c.{metric}{device} for c in data]")
-    clean_metrics = [item for item in metrics if item is not None]
-    if metrics[0] != None:
-        fig = px.line(x=dates, y=metrics)
-        limits = [2500, 4000]
-        colors = ['yellow', 'red']
-        # add color stripes
-        fig.add_shape(
-            type='rect',
-            x0=min(dates), x1=max(dates),
-            y0=0, y1=2500,
-            yref='y',
-            fillcolor='lightgreen',
-            layer='below',
-            opacity=0.4,
-            line_width=0
-        )
-        # TODO add color stripes - fig.add_shape
-        fig.update_layout(
-            title={
-                'text': f"{metric}",
-                'font': {'size': 24}
-            },
-
-            yaxis_range=[min(clean_metrics) - 0.5, max(clean_metrics) + 0.5],
-        )
-        config = dict({'modeBarButtonsToRemove': ['autoScale', 'zoom', 'pan', 'select', 'zoomIn', 'zoomOut']})
-        chart3 = fig.to_html(config=config)
-    else:
-        chart3 = "Data nejsou k dispozici"
-    
+    chart1 = create_plot(data, "lcp", device)
+    chart2 = create_plot(data, "fid", device)
+    chart3 = create_plot(data, "cls", device)
     return render(request, "dashboard/dashboard.html",
                       context={'chart1': chart1,'chart2': chart2, 'chart3': chart3, 'web_name': web_name,
                                'device': device, "url_obj": url_object, 'url_added': url_added})
