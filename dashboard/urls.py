9604a699ac2db23ad5441a9bbe606c36f42e789d
Tomas Kara <tomas.kara.91@gmail.com>
2023-03-26 18:22:05 +0200

    remove page functionality

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
-from dashboard.views import dashboard
+from dashboard.views import dashboard, remove_profile_url, add_profile_url
+from speedcheck.views import change_value
 
 
 urlpatterns = [
     path('dashboard/<int:url_id>/', dashboard, name='dashboard'),
+    path('remove-value/', remove_profile_url, name='remove_value'),
+    path('add-value/', add_profile_url, name='add_value'),
 
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 import plotly.express as px
-from speedcheck.models import Urls, CruxHistory, Profile
+from speedcheck.models import Urls, CruxHistory, Profile, ProfileUrl
 from .functions import create_plot
+from django.http import JsonResponse
 
 
 def dashboard(request, url_id):
     url_object = Urls.objects.get(id=url_id)
     web_name = url_object.url
     data = CruxHistory.objects.filter(url=url_id)
-    profile = Profile.objects.filter(user=request.user).first()
-    url_added = profile.urls.filter(url=web_name)
+    url_added = None
+    profile_url_id = None
+    if request.user.is_authenticated:
+        profile = Profile.objects.filter(user=request.user).first()
+        url_added = profile.urls.filter(url=web_name).first()
+    if url_added:
+        profile_url_id = ProfileUrl.objects.get(url=url_id, profile__user=request.user).id
     if request.POST.get('mobile'):
         device = request.POST.get('mobile')
 
@@ -18,8 +24,8 @@ def dashboard(request, url_id):
     else:
         device = "m"
 
-    if request.POST.get('addurl'):
-        request.user.profile.urls.add(url_object)
+    # if request.POST.get('addurl'):
+        # request.user.profile.urls.add(url_object)
     config = dict({'modeBarButtonsToRemove': ['autoScale', 'zoom', 'pan', 'select', 'zoomIn', 'zoomOut']})
     chart1 = create_plot(data, "lcp", device)
     if type(chart1) != str:
@@ -33,7 +39,21 @@ def dashboard(request, url_id):
     if type(chart3) != str:
         chart3 = chart3.to_html(config=config, include_plotlyjs='cdn')
     return render(request, "dashboard/dashboard.html",
-                      context={'chart1': chart1,'chart2': chart2, 'chart3': chart3, 'web_name': web_name,
-                               'device': device, "url_obj": url_object, 'url_added': url_added})
+                      context={'chart1': chart1, 'chart2': chart2, 'chart3': chart3, 'web_name': web_name,
+                               'device': device, "url_obj": url_object, 'url_added': url_added, 'profile_url_id': profile_url_id})
+
+
+def remove_profile_url(request):
+    url_id = int(request.GET.get('url_id'))
+    profile_url_object = ProfileUrl.objects.get(profile__user=request.user, url_id=url_id)
+    profile_url_object.delete()
+    return JsonResponse({'success': True})
+
+
+def add_profile_url(request):
+    url_id = int(request.GET.get('url_id'))
+    url_object = Urls.objects.get(id=url_id)
+    request.user.profile.urls.add(url_object)
+    return JsonResponse({'success': True})
 
 
