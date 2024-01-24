371d842dafda7264df065743511fd0abed318f3e
tomaskara <95649007+tomaskara@users.noreply.github.com>
2024-01-24 21:17:23 +0100

    change redirect for url_generator to 301 status

--- utm_generator/views.py
+++ utm_generator/views.py
@@ -7,4 +7,4 @@ def utm_builder(request):
     return render(request, template_name="utm_generator/utm_builder.html", context={"form": form})
 
 def utm_builder_redirect(request):
-    return redirect(utm_builder)
+    return redirect(utm_builder, permanent=True)
