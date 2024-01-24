97fe1af9ff3fce7d665be78d2644e10d36612d19
tomaskara <95649007+tomaskara@users.noreply.github.com>
2024-01-24 21:11:35 +0100

    fix url for utm-builder (set redirection) and some meta title edits

--- utm_generator/urls.py
+++ utm_generator/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import path, re_path
+from django.shortcuts import redirect
 
-from utm_generator.views import utm_builder
+from utm_generator.views import utm_builder, utm_builder_redirect
 
 urlpatterns = [
-    path("utm_generator/", utm_builder, name="utm_generator"),
+    path("utm_generator/", utm_builder_redirect, name='utm_generator_redirect'),
+    path("utm-generator/", utm_builder, name="utm_generator"),
 ]
 
 if settings.DEBUG:
--- utm_generator/views.py
+++ utm_generator/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UtmBuilder
 
 
 def utm_builder(request):
     form = UtmBuilder()
     return render(request, template_name="utm_generator/utm_builder.html", context={"form": form})
+
+def utm_builder_redirect(request):
+    return redirect(utm_builder)
